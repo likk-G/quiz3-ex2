56c276f7f591c175478362d5b6cd3cfac1005f1b
likk-G <83183455+likk-G@users.noreply.github.com>
2023-04-23 19:06:59 +0400

    Add files via upload

---
+++ CustomQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise2
+{
+    public class queue<T>
+    {
+        List<T> _queue;
+
+
+        public queue()
+        {
+            _queue = new List<T>();
+        }
+
+        public queue(T firstElement)
+        {
+            _queue = new List<T> { firstElement };
+        }
+        
+        public void Add(T element) { _queue.Add(element); }
+        public T Remove() 
+        { 
+            T _object = _queue[0];
+            _queue.RemoveAt(0); 
+            return _object;
+        }
+
+        public T Peek()
+        {
+            return _queue[0];
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using exercise2;
+using System.Globalization;
+
+queue<int> q1 = new();
+q1.Add(4);
+q1.Add(5);
+q1.Add(2);
+
+Console.WriteLine(q1.Peek());
+
+q1.Remove();
+
+Console.WriteLine(q1.Peek());
+
+
+
+ 
+
+
